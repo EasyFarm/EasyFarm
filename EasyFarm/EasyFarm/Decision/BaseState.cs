3248818c12df6b29e502a9d8f9bbec19f7b1eeea
Mykezero <as482jtm45@aol.com>
2014-02-08 01:33:11 -0500

    Forgot to add Decision folder; It holds the FSM Stuff.

---
+++ EasyFarm/EasyFarm/Decision/BaseState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using EasyFarm.Engine;
+
+public abstract class BaseState : IComparable<BaseState>
+{
+    public bool Enabled;
+    public int Priority;
+    protected GameEngine gameEngine;
+    
+    public abstract bool CheckState();
+    public abstract void EnterState();
+    public abstract void RunState();
+    public abstract void ExitState();
+
+    public BaseState(ref GameEngine gameEngine)
+    {
+        this.gameEngine = gameEngine;
+    }
+
+    public int CompareTo(BaseState other)
+    {
+        return -this.Priority.CompareTo(other.Priority);
+    }
+}
---
+++ EasyFarm/EasyFarm/Decision/RestState.cs
@@ -0,0 +1,29 @@
+﻿using EasyFarm.Engine;
+
+class RestState : BaseState
+{
+    public RestState(ref GameEngine gameEngine) : base(ref gameEngine) { }
+
+    public override bool CheckState()
+    {
+        return gameEngine.Player.shouldRest && gameEngine.IsWorking;
+    }
+
+    public override void EnterState()
+    {
+
+    }
+
+    public override void RunState()
+    {
+        if (!gameEngine.Player.IsResting)
+        {
+            gameEngine.Player.RestingOn();
+        }
+    }
+
+    public override void ExitState()
+    {
+
+    }
+}
