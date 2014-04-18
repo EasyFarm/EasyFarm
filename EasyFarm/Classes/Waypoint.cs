2e045a0693ac52a3099deb468c3548cfea6cc9c8
Mykezero <as482jtm45@aol.com>
2014-04-18 18:48:07 -0400

    Changed waypoint to waypoint<>

--- EasyFarm/Classes/Waypoint.cs
+++ EasyFarm/Classes/Waypoint.cs
@@ -1,30 +1,12 @@
-
-/*///////////////////////////////////////////////////////////////////
-<EasyFarm, general farming utility for FFXI.>
-Copyright (C) <2013 - 2014>  <Zerolimits>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-*////////////////////////////////////////////////////////////////////
-
 ﻿using FFACETools;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace EasyFarm.PathingTools
+namespace EasyFarm.ViewModels
 {
-    public class Waypoint
+    class Waypoint<T>
     {
         public FFACE.Position Position { get; set; }
 
