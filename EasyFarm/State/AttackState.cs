b36d6a69084a9e53d2fc1059d98df03c722f49a7
Mike <as482jtm45@aol.com>
2014-09-19 00:40:05 -0400

    Merging SchroedingerHepcat's fixes into the program. I've made slight modifications to them by replacing the for loops with linq queries and simplifing some of the conditionals.

--- EasyFarm/State/AttackState.cs
+++ EasyFarm/State/AttackState.cs
@@ -14,13 +14,15 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-*////////////////////////////////////////////////////////////////////
+*/
+///////////////////////////////////////////////////////////////////
 
 
 using FFACETools;
 using System.Threading;
 using ZeroLimits.FarmingTool;
 using ZeroLimits.XITools;
+using System.Linq;
 
 namespace EasyFarm.State
 {
@@ -48,73 +50,58 @@ namespace EasyFarm.State
             // Get the target
             var target = ftools.TargetData.TargetUnit;
 
-            bool offensiveSpellsInStartList = false;
-            bool rangedInStartList = false;
+            /* 
+             * I've replaced the for loops with the linq queries.
+             * It combines the two lists and then checks for the requirements the same as before. 
+             */ 
+            
+            // True when there exists an ability that is an offensive spell.
+            bool offensiveInLists = ftools.PlayerActions.StartList
+                .Union(ftools.PlayerActions.PullList)
+                .Any(x => x.IsSpell && x.Postfix == "<t>");
+            
+            // True when there exists an ability that is a ranged attack. 
+            bool rangedInLists = ftools.PlayerActions.StartList
+                .Union(ftools.PlayerActions.PullList)
+                .Any(x => x.Prefix == "/range");
 
-            bool offensiveSpellsInPullList = false;
-            bool rangedInPullList = false;
-
-            bool offensiveSpellsInStartOrPullList = false;
-            bool rangedInStartOrPullList = false;
-
-            for (int i = 0; i < ftools.PlayerActions.StartList.Count; ++i)
-            {
-                offensiveSpellsInStartList |= (  ftools.PlayerActions.StartList[i].IsSpell
-                                              && ftools.PlayerActions.StartList[i].Postfix == "<t>"
-                                              );
-                rangedInStartList |= (ftools.PlayerActions.StartList[i].Prefix == "/range");
-            }
-
-            for (int i = 0; i < ftools.PlayerActions.PullList.Count; ++i)
-            {
-                offensiveSpellsInPullList |= (  ftools.PlayerActions.PullList[i].IsSpell
-                                             && ftools.PlayerActions.PullList[i].Postfix == "<t>"
-                                             );
-                rangedInPullList |= (ftools.PlayerActions.PullList[i].Prefix == "/range");
-            }
-
-            offensiveSpellsInStartOrPullList = (offensiveSpellsInPullList || offensiveSpellsInStartList);
-            rangedInStartOrPullList = (rangedInPullList || rangedInStartList);
-
+            // The our new pull distance determined by what moves 
+            // we have in the list.
             double startPullDistance;
-            if (offensiveSpellsInStartOrPullList)
-            {
-                startPullDistance = Constants.SPELL_CAST_DISTANCE;
-            }
-            else if (rangedInStartOrPullList)
-            {
-                startPullDistance = Constants.RANGED_ATTACK_MAX_DISTANCE;
-            }
-            else
-            {
-                startPullDistance = fface.Navigator.DistanceTo(target.Position);
-            }
 
+            // If we have a spell in the lists, set pull distance to spell distance. 
+            if (offensiveInLists) { startPullDistance = Constants.SPELL_CAST_DISTANCE; }
+            // If we have a ranged attack in the lists, set pull distance to ranged distance. 
+            else if (rangedInLists) { startPullDistance = Constants.RANGED_ATTACK_MAX_DISTANCE; }
+            // Set pull distance to target's distance. 
+            else { startPullDistance = fface.Navigator.DistanceTo(target.Position); }
+
+            // If the target is out of range move into range.
             if (fface.Navigator.DistanceTo(target.Position) > startPullDistance)
             {
                 // Save old tolerance
                 var old = fface.Navigator.DistanceTolerance;
-                
+
                 // Set to max engagement distance.
                 fface.Navigator.DistanceTolerance = startPullDistance;
 
                 // Goto target at max engagement distance.
                 fface.Navigator.Goto(target.Position, false);
-                
+
                 // Restore old tolerance. 
                 fface.Navigator.DistanceTolerance = old;
             }
-            
+
             // Face the target
             fface.Navigator.FaceHeading(target.ID);
-            
+
             // Target the target
             if (target.ID != fface.Target.ID)
                 ftools.CombatService.TargetUnit(target);
 
             // Check correct target
             if (target.ID != fface.Target.ID) return;
-            
+
             /* 
              * Cast the starting moves. Used to buff the character. 
              * Will cast until all spells are successfully casted.  
@@ -132,39 +119,32 @@ namespace EasyFarm.State
             // set to true so that we do not cast starting spells again. 
             fightStarted = true;
 
-            if (ftools.PlayerActions.PullList.Count > 0 && (rangedInPullList || offensiveSpellsInPullList))
+            // If the pull list is not empty and we have a ranged or offensive move to use (prefix = <t>)
+            if (ftools.PlayerActions.PullList.Count > 0 && (rangedInLists|| offensiveInLists))
             {
                 // Pull the target casting each spell once until the target is claimed
                 while (!target.Status.Equals(Status.Fighting) && !target.IsDead)
                 {
                     // If out of range, stop trying to pull
-                    if (fface.Navigator.DistanceTo(target.Position) > startPullDistance)
-                    {
-                        return;
-                    }
+                    if (fface.Navigator.DistanceTo(target.Position) > startPullDistance) { return; }
                     else
                     {
-                        ftools.AbilityExecutor.ExecuteActions(target
-                                                             ,ftools.PlayerActions.PullList
-                                                             ,Constants.SPELL_CAST_LATENCY
-                                                             ,Constants.GLOBAL_SPELL_COOLDOWN
-                                                             );
+                        ftools.AbilityExecutor.ExecuteActions(target, ftools.PlayerActions.PullList,
+                            Constants.SPELL_CAST_LATENCY, Constants.GLOBAL_SPELL_COOLDOWN);
                     }
                 }
             }
+
             else if (!target.Status.Equals(Status.Fighting))
             {
-                ftools.AbilityExecutor.ExecuteActions(target
-                                                     ,ftools.PlayerActions.PullList
-                                                     ,Constants.SPELL_CAST_LATENCY
-                                                     ,Constants.GLOBAL_SPELL_COOLDOWN
-                                                     );
+                ftools.AbilityExecutor.ExecuteActions(target, ftools.PlayerActions.PullList, 
+                    Constants.SPELL_CAST_LATENCY, Constants.GLOBAL_SPELL_COOLDOWN);
             }
 
             // *Removed: target does not have to be engaged for us to fight it. *
             // Check pull
             // if (!target.Status.Equals(Status.Fighting)) return;
-            
+
             // Engage the target
             if (!fface.Player.Status.Equals(Status.Fighting))
             {
@@ -172,8 +152,8 @@ namespace EasyFarm.State
             }
 
             // Check engaged
-            if (!fface.Player.Status.Equals(Status.Fighting)) return; 
-            
+            if (!fface.Player.Status.Equals(Status.Fighting)) return;
+
             // Move to the target
             /*
              * Fixed bug that used Constants.MeleeDistance instead of the user 
@@ -185,7 +165,7 @@ namespace EasyFarm.State
             if (fface.Navigator.DistanceTo(target.Position) > ftools.UserSettings.MiscSettings.MeleeDistance)
             {
                 var old = fface.Navigator.DistanceTolerance;
-                fface.Navigator.DistanceTolerance = ftools.UserSettings.MiscSettings.MeleeDistance;                
+                fface.Navigator.DistanceTolerance = ftools.UserSettings.MiscSettings.MeleeDistance;
                 fface.Navigator.GotoNPC(target.ID);
                 fface.Navigator.DistanceTolerance = old;
             }
