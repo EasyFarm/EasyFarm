b42e064702cdfa8dec6ce32107d6a0b3ac01cd56
Mike <as482jtm45@aol.com>
2014-12-26 19:20:48 -0500

    Updated GPL headers for new files.

--- XML Filter/Form1.Designer.cs
+++ XML Filter/Form1.Designer.cs
@@ -1,4 +1,23 @@
-﻿namespace XML_Filter
+﻿
+/*///////////////////////////////////////////////////////////////////
+<EasyFarm, general farming utility for FFXI.>
+Copyright (C) <2013>  <Zerolimits>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+*/
+///////////////////////////////////////////////////////////////////
+
+namespace XML_Filter
 {
     partial class Form1
     {
@@ -124,7 +143,6 @@
             this.Controls.Add(this.AttributeContentListBox);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- XML Filter/Form1.cs
+++ XML Filter/Form1.cs
@@ -1,3 +1,22 @@
+
+/*///////////////////////////////////////////////////////////////////
+<EasyFarm, general farming utility for FFXI.>
+Copyright (C) <2013>  <Zerolimits>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+*/
+///////////////////////////////////////////////////////////////////
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,13 +34,6 @@ namespace XML_Filter
 {
     public partial class Form1 : Form
     {
-        public class Constants
-        {
-            public static String RESOURCE_FOLDER_NAME = "resources";
-            public static String ABILITY_FILE_NAME = "abils.xml";
-            public static String SPELL_FILE_NAME = "spells.xml";
-        }
-
         public Form1()
         {
             InitializeComponent();
@@ -80,11 +92,6 @@ namespace XML_Filter
             ofd.InitialDirectory = Directory.GetCurrentDirectory();
             ofd.ShowDialog();
             FileNameTextBox.Text = ofd.FileName;
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
