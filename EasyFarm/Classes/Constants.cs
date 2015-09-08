f029ca626159f48f6d242df1f04b9b9dc1888b3e
Mykezero <as482jtm45@aol.com>
2015-09-08 08:13:04 -0400

    Removed unused constants.

--- EasyFarm/Classes/Constants.cs
+++ EasyFarm/Classes/Constants.cs
@@ -21,103 +21,58 @@ namespace EasyFarm.Classes
     public class Constants
     {
         /// <summary>
-        ///     Command for disengaging
+        /// Command for disengaging
         /// </summary>
         public const string AttackOff = "/attack off";
 
         /// <summary>
-        ///     Command for engaging
+        /// Command for engaging
         /// </summary>
         public const string AttackTarget = "/attack <t>";
 
         /// <summary>
-        ///     Command for engaging a target
-        /// </summary>
-        public const string AttackOn = "/attack on";
-
-        /// <summary>
-        ///     Max time used to spend running to a mob before timing out.
-        /// </summary>
-        public const int RunDuration = 3;
-
-        /// <summary>
-        ///     Min distance to stand when attacking an opponent.
+        /// Min distance to stand when attacking an opponent.
         /// </summary>
         public const int MeleeDistance = 3;
 
         /// <summary>
-        ///     Max difference an attackable mob can be before being considered unreachable.
+        /// Max difference an attackable mob can be before being considered unreachable.
         /// </summary>
         public const int HeightThreshold = 5;
 
         /// <summary>
-        ///     The max distance a mob will be recoginized by the bot.
+        /// The max distance a mob will be recoginized by the bot.
         /// </summary>
         public const double DetectionDistance = 17;
 
         /// <summary>
-        ///     The minimum amount of tp needed to perform a weapon skill.
-        /// </summary>
-        public const int WeaponskillTp = 1000;
-
-        /// <summary>
-        ///     Command for resting
+        /// Command for resting
         /// </summary>
         public const string RestingOn = "/heal on";
 
         /// <summary>
-        ///     Command for stopping resting
+        /// Command for stopping resting
         /// </summary>
         public const string RestingOff = "/heal off";
 
         /// <summary>
-        ///     The amount of time to wait to recast any spell that has failed to
-        ///     help prevent spamming pulling moves.
-        /// </summary>
-        public const int PullSpellRecastDuration = 0;
-
-        /// <summary>
-        ///     One second for spells to fire when casted through
-        ///     WindowerTools.SendString(command).
-        /// </summary>
-        public const int SpellCastLatency = 1000;
-
-        /// <summary>
-        ///     Each spell takes 5 seconds to cast after a previous spell
-        ///     has been casted.
+        /// Each spell takes 5 seconds to cast after a previous spell has been casted.
         /// </summary>
         public const int GlobalSpellCooldown = 1000;
 
         /// <summary>
-        ///     Maximum range a spell may be casted.
-        /// </summary>
-        public const int SpellCastDistance = 21;
-
-        /// <summary>
-        ///     Maximum range for ranged attack.
-        /// </summary>
-        public const int RangedAttackMaxDistance = 25;
-
-        /// <summary>
-        ///     The upper limit of the spawn array. (Monsters, NPCs, Players)
+        /// The upper limit of the spawn array. (Monsters, NPCs, Players)
         /// </summary>
         public const int UnitArrayMax = 2048;
 
         /// <summary>
-        ///     The upper limit of the mob array. (Monsters, NPCS)
+        /// The upper limit of the mob array. (Monsters, NPCS)
         /// </summary>
         public const int MobArrayMax = 768;
 
         /// <summary>
-        ///     Controls the wait time between checks on the
-        ///     unit array to control performance.
+        /// Controls the wait time between checks on the unit array to control performance.
         /// </summary>
         public const double UnitArrayCheckRate = 1;
-    }
-
-    public class EnsureCastConstants
-    {
-        public const int SpellRecastAttempts = 3;
-        public const double SpellRecastDelay = 0;
     }
 }
