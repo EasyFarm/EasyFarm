4a9442b04f110be934422ab579ec953401b007f9
Mike Bartron <mikebartron@gmail.com>
2015-12-12 22:05:30 -0500

    Removed cruft from targeting.

--- MemoryAPI/Target/ITargetTools.cs
+++ MemoryAPI/Target/ITargetTools.cs
@@ -2,24 +2,7 @@
 {
     public interface ITargetTools
     {
-        short HPPCurrent { get; }
         int ID { get; }
-        bool IsLocked { get; }
-        bool IsSub { get; }
-        ushort Mask { get; }
-        string Name { get; }
-        float PosH { get; }
-        IPosition Position { get; }
-        float PosX { get; }
-        float PosY { get; }
-        float PosZ { get; }
-        int ServerID { get; }
-        Status Status { get; }
-        int SubID { get; }
-        ushort SubMask { get; }
-        int SubServerID { get; }
-        NPCType Type { get; }
-
         bool SetNPCTarget(int index);
     }
 }
