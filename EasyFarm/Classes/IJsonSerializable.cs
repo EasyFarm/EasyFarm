bfa360e979cb43e40764235a4da0324d3a6e860e
Joshua Tyree <joshtyree.90@gmail.com>
2017-04-27 14:14:39 -0500

    Latest

---
+++ EasyFarm/Classes/IJsonSerializable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyFarm.Classes {
+    public interface IJsonSerializable {
+        void PrepareForSerialization();
+        void CompleteDeserialization(); 
+    }
+}
