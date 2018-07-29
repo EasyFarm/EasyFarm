1d2108541cf956fcdc9a20381691258c0f7c13c1
Mykezero <Mykezero@users.noreply.github.com>
2018-07-29 15:09:28 -0400

    Centralize build settings to config file

--- Build/scripts/Run-Tests.ps1
+++ Build/scripts/Run-Tests.ps1
@@ -1,14 +1,5 @@
-###############################################################################
-## Configuration
-###############################################################################
 Param([string]$configuration="Release")
-$FindParent = "./Find-Parent.ps1"
-$bin_folder= (&$FindParent "EasyFarm") + "\EasyFarm.Tests\bin\$configuration"
-$test_runner = (&$FindParent "EasyFarm") + "\packages\xunit.runner.console.*\tools\net452\xunit.console.x86.exe"
-$test_assembly = "$bin_folder\EasyFarm.Tests.dll"
 
-###############################################################################
+. ./Config.ps1
 
 &$test_runner $test_assembly
-
-###############################################################################
