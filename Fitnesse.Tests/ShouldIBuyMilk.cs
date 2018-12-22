b4ec18816268d21e987a5346c14cd45b190e78af
Mykezero <Mykezero@users.noreply.github.com>
2018-12-22 18:28:14 -0500

    Get first fitnesse test working

---
+++ Fitnesse.Tests/ShouldIBuyMilk.cs
@@ -0,0 +1,18 @@
+﻿using fit;
+
+namespace Fitnesse.Tests
+{
+    public class ShouldIBuyMilk : ColumnFixture
+    {
+        public int CashInWallet;
+        public int PintsOfMilkRemaining;
+        public string CreditCard;
+
+        public string GoToStore()
+        {
+            if (CashInWallet > 0 || CreditCard.Equals("yes"))
+                return "yes";
+            return "no";
+        }
+    }
+}
