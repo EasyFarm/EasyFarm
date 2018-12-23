79f44cee0ea582b2652329a95dcbdf695a2ecf03
Mykezero <Mykezero@users.noreply.github.com>
2018-12-22 19:39:56 -0500

    Add tests for TargetSelection

---
+++ Fitnesse.Tests/Fixtures/UtilityAI/TargetSelection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using fit;
+
+namespace Fitnesse.Tests.Fixtures.UtilityAI
+{
+    public class TargetSelection : ColumnFixture
+    {
+        public decimal IsHoldingGrenade { get; set; }
+        public decimal SniperAngle { get; set; }
+        public decimal GunnerDistance { get; set; }
+        public decimal GrenadierDistance { get; set; }
+        public decimal SniperDistance { get; set; }
+
+        public decimal Grenadier()
+        {
+            return 0;
+        }
+
+        public decimal Sniper()
+        {
+            return 0;
+        }
+
+        public decimal Gunner()
+        {
+            return 0;
+        }
+
+        public String Result()
+        {
+            return "None";
+        }
+    }
+}
