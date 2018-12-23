b788fcb906b08161da034aab64b9e626404a3131
Mykezero <Mykezero@users.noreply.github.com>
2018-12-22 20:38:17 -0500

    WIP: World uses Gunner distance for ProxityScorer - Should vary per job

---
+++ Fitnesse.Tests/Fixtures/UtilityAI/IsSniperPointingTowardsUs.cs
@@ -0,0 +1,20 @@
+﻿namespace Fitnesse.Tests.Fixtures.UtilityAI
+{
+    public sealed class IsSniperPointingTowardsUs
+    {
+        private readonly GameWorld _world;
+        private readonly decimal score = 40;
+        private readonly decimal maxAngle = 30;
+
+        public IsSniperPointingTowardsUs(GameWorld world)
+        {
+            _world = world;
+        }
+
+        public decimal Score()
+        {
+            decimal angle = _world.SniperAngle;
+            return angle < maxAngle ? score : 0;
+        }
+    }
+}
