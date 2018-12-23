97bdfb4252e8ac00a62cd21fb88eddae1d00e47a
Mykezero <Mykezero@users.noreply.github.com>
2018-12-22 20:09:12 -0500

    Start implementing grenadier tests

--- Fitnesse.Tests/Fixtures/UtilityAI/TargetSelection.cs
+++ Fitnesse.Tests/Fixtures/UtilityAI/TargetSelection.cs
@@ -5,15 +5,29 @@ namespace Fitnesse.Tests.Fixtures.UtilityAI
 {
     public class TargetSelection : ColumnFixture
     {
-        public decimal IsHoldingGrenade { get; set; }
+        private decimal _grenadierScore;        
+
+        public void Execute()
+        {
+            var world = new GameWorld()
+            {
+                IsHoldingGrenade = IsHoldingGrenade
+            };
+            var scorer = new IsGrenadierHoldingGrenade(world);
+            _grenadierScore = scorer.Score();
+        }
+
+        public bool IsHoldingGrenade { get; set; }
         public decimal SniperAngle { get; set; }
         public decimal GunnerDistance { get; set; }
         public decimal GrenadierDistance { get; set; }
         public decimal SniperDistance { get; set; }
 
+
         public decimal Grenadier()
         {
-            return 0;
+            Execute();
+            return _grenadierScore;
         }
 
         public decimal Sniper()
@@ -30,5 +44,27 @@ namespace Fitnesse.Tests.Fixtures.UtilityAI
         {
             return "None";
         }
+    }
+
+    public class IsGrenadierHoldingGrenade
+    {
+        private readonly GameWorld _world;
+
+        private decimal _score = 50;
+
+        public IsGrenadierHoldingGrenade(GameWorld world)
+        {
+            _world = world;
+        }
+
+        public decimal Score()
+        {
+            return _world.IsHoldingGrenade ? _score : 0;
+        }
+    }
+
+    public class GameWorld
+    {
+        public bool IsHoldingGrenade { get; set; }
     }
 }
