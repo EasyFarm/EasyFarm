543e3f8eb303358f2dbe7aa838450327726582b3
Mykezero <Mykezero@users.noreply.github.com>
2019-01-05 00:07:53 -0500

    Clean up DeadStateTests

--- EasyFarm.Tests/States/DeadStateTests.cs
+++ EasyFarm.Tests/States/DeadStateTests.cs
@@ -26,95 +26,72 @@ using Xunit;
 
 namespace EasyFarm.Tests.States
 {
-    public class DeadStateTests
+    public class DeadStateTests : AbstractTestBase
     {
-        public class CheckTests
-        {
-            [Theory]
-            [InlineData(Status.Dead1, true)]
-            [InlineData(Status.Dead2, true)]
-            [InlineData(Status.Standing, false)]
-            [InlineData(Status.Fighting, false)]
-            public void CheckTrueWhenPlayersDead(Status status, bool expected)
-            {
-                // Setup fixture
-                var context = new TestContext();
-                context.Player.Status = status;
-                var sut = CreateSut();
-                // Exercise system
-                var result = sut.Check(context);
-                // Verify outcome
-                Assert.Equal(expected, result);
-                // Teardown
-            }
+        private static readonly TestContext context = new TestContext();
+        private static readonly DeadState sut = new DeadState();
 
-            private DeadState CreateSut()
-            {
-                return new DeadState();
-            }
+        [Theory]
+        [InlineData(Status.Dead1, true)]
+        [InlineData(Status.Dead2, true)]
+        [InlineData(Status.Standing, false)]
+        [InlineData(Status.Fighting, false)]
+        public void CheckTrueWhenPlayersDead(Status status, bool expected)
+        {
+            // Setup fixture
+            context.Player.Status = status;
+            // Exercise system
+            bool result = sut.Check(context);
+            // Verify outcome
+            Assert.Equal(expected, result);
+            // Teardown
         }
 
-        public class RunTests : AbstractTestBase
+        [Fact]
+        public void RunResetIsCalledToStopPlayerFromRunning()
         {
-            [Fact]
-            public void RunResetIsCalledToStopPlayerFromRunning()
-            {
-                // Setup fixture
-                var context = new TestContext();
-                context.MockAPI.Navigator.IsRunning = true;
-                var sut = CreateSut();
-                // Exercise system
-                sut.Run(context);
-                // Verify outcome
-                Assert.False(context.MockAPI.Navigator.IsRunning);
-                // Teardown
-            }
+            // Setup fixture
+            context.MockAPI.Navigator.IsRunning = true;
+            // Exercise system
+            sut.Run(context);
+            // Verify outcome
+            Assert.False(context.MockAPI.Navigator.IsRunning);
+            // Teardown
+        }
 
-            [Fact]
-            public void RunDoesNotHomePointIfConfigNotSet()
-            {
-                // Setup fixture
-                var context = new TestContext();
-                var sut = CreateSut();
-                // Exercise system
-                sut.Run(context);
-                // Verify outcome
-                Assert.Empty(context.MockAPI.Windower.KeyPresses);
-                // Teardown
-            }
+        [Fact]
+        public void RunDoesNotHomePointIfConfigNotSet()
+        {
+            // Setup fixture
+            // Exercise system
+            sut.Run(context);
+            // Verify outcome
+            Assert.Empty(context.MockAPI.Windower.KeyPresses);
+            // Teardown
+        }
 
-            [Fact]
-            public void RunSendsPauseCommand()
-            {
-                // Setup fixture
-                var context = new TestContext();
-                var sut = CreateSut();
-                // Exercise system
-                sut.Run(context);
-                // Verify outcome
-                Assert.Contains(typeof(Events.PauseEvent), Events);
-                // Teardown
-            }
+        [Fact]
+        public void RunSendsPauseCommand()
+        {
+            // Setup fixture
+            // Exercise system
+            sut.Run(context);
+            // Verify outcome
+            Assert.Contains(typeof(Events.PauseEvent), Events);
+            // Teardown
+        }
 
-            [Fact]
-            public void RunAttemptsToHomepointAfterDeath()
-            {
-                // Setup fixture
-                var context = new TestContext();
-                context.Config.HomePointOnDeath = true;
-                var sut = CreateSut();
-                var expected = new List<Keys> {Keys.NUMPADENTER, Keys.LEFT, Keys.NUMPADENTER};
-                // Exercise system
-                sut.Run(context);
-                // Verify outcome
-                Assert.Equal(expected, context.MockAPI.Windower.KeyPresses);
-                // Teardown
-            }
-
-            private DeadState CreateSut()
-            {
-                return new DeadState();
-            }
+        [Fact]
+        public void RunAttemptsToHomepointAfterDeath()
+        {
+            // Setup fixture
+            context.Config.HomePointOnDeath = true;
+            List<Keys> expected = new List<Keys> { Keys.NUMPADENTER, Keys.LEFT, Keys.NUMPADENTER };
+            // Exercise system
+            sut.Run(context);
+            // Verify outcome
+            Assert.Equal(expected, context.MockAPI.Windower.KeyPresses);
+            // Teardown
         }
     }
 }
