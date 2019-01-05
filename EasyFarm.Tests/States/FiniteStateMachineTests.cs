9840367967e96d7c9a49209ac6455d00a35c908a
Mykezero <Mykezero@users.noreply.github.com>
2019-01-05 15:48:57 -0500

    Add first FSM test

---
+++ EasyFarm.Tests/States/FiniteStateMachineTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using EasyFarm.States;
+using EasyFarm.Tests.Context;
+using Xunit;
+
+namespace EasyFarm.Tests.States
+{
+    public class FiniteStateMachineTests
+    {
+        [Fact]
+        public void OnFirstRun_StartStateWillRun()
+        {
+            // Fixture setup
+            TestContext context = new TestContext();
+            NewFiniteStateMachine sut = new NewFiniteStateMachine();
+            StartState startState = new StartState();
+            sut.AddState(startState);
+            // Exercise system
+            IList<IState> result = sut.Run();
+            // Verify outcome
+            Assert.Contains(startState, result);
+            // Teardown	
+        }
+    }
+
+    public class NewFiniteStateMachine
+    {
+        private readonly IList<IState> _states = new List<IState>();
+
+        public void AddState(IState state)
+        {
+            _states.Add(state);
+        }
+
+        public IList<IState> Run()
+        {
+            return _states;
+        }
+    }
+}
