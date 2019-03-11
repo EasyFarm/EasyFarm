2f8ae5b7dcae458fa8ae4c4cb2cfa4491168ad70
Mykezero <Mykezero@users.noreply.github.com>
2019-03-11 09:13:45 -0400

    Fix duplicate follow tabs in creating the tab view model

--- EasyFarm/Infrastructure/CompositeRoot.cs
+++ EasyFarm/Infrastructure/CompositeRoot.cs
@@ -66,7 +66,7 @@ namespace EasyFarm.Infrastructure
             TabViewModels tabViewModels = new TabViewModels(new List<IViewModel>()
             {
                 battlesViewModel,
-                followViewModel,
+                targetingViewModel,
                 restingViewModel,
                 routesViewModel,
                 followViewModel,
