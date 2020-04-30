ad3363d20a0296579182aa4662b4e522dbd9f7cd
Jonathan Willis <jondwillis@gmail.com>
2020-04-30 15:37:05 -0400

    Fixing tests

--- EasyFarm.Tests/Context/MockPlayer.cs
+++ EasyFarm.Tests/Context/MockPlayer.cs
@@ -27,6 +27,7 @@ namespace EasyFarm.Tests.Context
         public bool HasAggro { get; set; }
         public Zone Zone { get; set; }
         public int Str { get; set; }
+        public string Name { get; set; }
         public int MppCurrent { get; set; }
     }
 }
