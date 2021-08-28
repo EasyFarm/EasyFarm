a41e951d5590a5b724dd03937b3e34baf521fcd3
SchroedingerHepcat <SchroedingerHepcat@gmail.com>
2021-08-28 10:19:06 -0600

    Remove cycling of battle stance for now as it doesn't seem needed.

--- EasyFarm/States/BattleState.cs
+++ EasyFarm/States/BattleState.cs
@@ -66,7 +66,7 @@ namespace EasyFarm.States
 
         public override void Run(IGameContext context)
         {
-            ShouldRecycleBattleStateCheck(context);
+            //ShouldRecycleBattleStateCheck(context);
             
             // Cast only one action to prevent blocking curing. 
             var action = context.Config.BattleLists["Battle"].Actions
